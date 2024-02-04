7739c70a3324350941cbad5100d357c904de8ac5
Danil Goncharov <dg444k@gmail.com>
2024-02-04 21:27:24 +0300

    fix bugs with picture

--- src/api/cardsDate.ts
+++ src/api/cardsDate.ts
@@ -1,66 +1,77 @@
-
+import Aang from "../assets/aang.jpg";
+import Appa from "../assets/appa.webp";
+import Azula from "../assets/azula.jpg";
+import Iroh from "../assets/iroh.webp";
+import Katara from "../assets/katara.jpg";
+import Momo from "../assets/momo.jpg";
+import Pakku from "../assets/pakku.webp";
+import Piandao from "../assets/piandao.webp";
+import Sokka from "../assets/sokka.png";
+import Suki from "../assets/suki.webp";
+import Toph from "../assets/toph.webp";
+import Zuko from "../assets/zuko.webp";
 
 const cardsDate = [
-    {
-        id: 1,
-        title: 'Aang',
-        src: '../../assets/aang.jpg',
-    },
-    {
-        id: 2,
-        title: 'Appa',
-        src: '../../assets/appa.webp',
-    },
-    {
-        id: 3,
-        title: 'Azula',
-        src: '../../assets/azula.jpg', 
-    },
-    {
-        id: 4,
-        title: 'Iroh',
-        src: '../../assets/iroh.webp',
-    },
-    {
-        id: 5,
-        title: 'Katara',
-        src: '../../assets/katara.jpg',
-    },
-    {
-        id: 6,
-        title: 'Momo',
-        src: '../../assets/momo.jpg',
-    },
-    {
-        id: 7,
-        title: 'Pakku',
-        src: '../../assets/pakku.webp',
-    },
-    {
-        id: 8,
-        title: 'Piandao',
-        src: '../../assets/piandao.webp',
-    },
-    {
-        id: 9,
-        title: 'Sokka',
-        src: '../../assets/sokka.png',
-    },
-    {
-        id: 10,
-        title: 'Suki',
-        src: '../../assets/suki.webp',
-    },
-    {
-        id: 11,
-        title: 'Toph',
-        src: '../../assets/toph.webp', 
-    },
-    {
-        id: 12,
-        title: 'Zuko',
-        src: '../../assets/zuko.webp',
-    },
+  {
+    id: 1,
+    title: "Aang",
+    src: Aang,
+  },
+  {
+    id: 2,
+    title: "Appa",
+    src: Appa,
+  },
+  {
+    id: 3,
+    title: "Azula",
+    src: Azula,
+  },
+  {
+    id: 4,
+    title: "Iroh",
+    src: Iroh,
+  },
+  {
+    id: 5,
+    title: "Katara",
+    src: Katara,
+  },
+  {
+    id: 6,
+    title: "Momo",
+    src: Momo,
+  },
+  {
+    id: 7,
+    title: "Pakku",
+    src: Pakku,
+  },
+  {
+    id: 8,
+    title: "Piandao",
+    src: Piandao,
+  },
+  {
+    id: 9,
+    title: "Sokka",
+    src: Sokka,
+  },
+  {
+    id: 10,
+    title: "Suki",
+    src: Suki,
+  },
+  {
+    id: 11,
+    title: "Toph",
+    src: Toph,
+  },
+  {
+    id: 12,
+    title: "Zuko",
+    src: Zuko,
+  },
 ];
 
 export default cardsDate;
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,39 +1,39 @@
-import './App.css';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Cards from '../Cards/Cards';
-import cardsDate from '../../api/cardsDate';
-import ScoreDisplay from '../Score/ScoreDisplay';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Cards from "../Cards/Cards";
+import cardsDate from "../../api/cardsDate";
+import ScoreDisplay from "../Score/ScoreDisplay";
+import { useEffect, useState } from "react";
 
 const App: React.FC = () => {
   const [isClick, setIsClick] = useState(0);
   const [bestScore, setBestScore] = useState(0);
 
-
   useEffect(() => {
-    alert('Welcome to the club body!');
+    alert("Welcome to the club body!");
   }, []);
 
-  function handleChangeScore (score: number): void {
-    setIsClick(score)
+  function handleChangeScore(score: number): void {
+    setIsClick(score);
   }
 
-  function handleChangeBestScore (bestScore: number): void {
-    setBestScore(bestScore)
+  function handleChangeBestScore(bestScore: number): void {
+    setBestScore(bestScore);
   }
- 
+
   return (
-    <div className='root'>
-      <Header/>
-      <ScoreDisplay score = {isClick} bestScore={bestScore} />
-      <Cards 
-        cards={cardsDate} 
-        onClick={handleChangeScore} 
-        updateBestScore={handleChangeBestScore} />
-      <Footer/>
+    <div className="root">
+      <Header />
+      <ScoreDisplay score={isClick} bestScore={bestScore} />
+      <Cards
+        cards={cardsDate}
+        onClick={handleChangeScore}
+        updateBestScore={handleChangeBestScore}
+      />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default App;
